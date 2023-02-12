53dd11b838d31bca9a6851742bf095482f1311bf
Jhanvi Mimani <jhanvi13mimani@gmail.com>
2023-02-12 11:48:09 +0530

    submitting

---
+++ src/main/java/org/antwalk/BeforeMethod.java
@@ -0,0 +1,17 @@
+package org.antwalk;
+
+import java.lang.reflect.Method;
+
+import org.springframework.aop.MethodBeforeAdvice;
+
+public class BeforeMethod implements MethodBeforeAdvice 
+{
+
+	@Override
+	public void before(Method method, Object[] args, Object target) throws Throwable
+	{
+		System.out.println("\nMarks");
+		
+	}
+	
+}
